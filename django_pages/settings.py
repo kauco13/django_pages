b5925544df8f256c8a4a41a2f2fd2d9997cf9109
Trey Aucoin <trey.aucoin@gmail.com>
2021-03-23 15:28:50 -0500

    configuring for heroku

--- django_pages/settings.py
+++ django_pages/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c1!x^b(*mus)3xqucpg=_#*izlfekh%ooys))+)klzgj2*oha+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages'
+    'pages' #new
 ]
 
 MIDDLEWARE = [
--- django_pages/urls.py
+++ django_pages/urls.py
@@ -29,10 +29,10 @@ urlpatterns = [
 ]
 
 # pages/urls.py
-from django.urls import path
-from .views import HomePageView, AboutPageView
-
-urlpatterns = [
-    path('about/', AboutPageView.as_view(), name='about'),
-    path('', HomePageView.as_view(), name='home'),
-]
+# from django.urls import path
+# from .views import HomePageView, AboutPageView
+#
+# urlpatterns = [
+#     path('about/', AboutPageView.as_view(), name='about'),
+#     path('', HomePageView.as_view(), name='home'),
+# ]
